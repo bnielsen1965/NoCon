0134dec54b9b145cfdb327ae77e34c0688a3df0a
burnin <bnielsen1965@gmail.com>
2015-04-11 16:38:22 -0700

    Move bootstrap sequence to script to enable use outside of application.

--- index.php
+++ index.php
@@ -19,23 +19,11 @@
 * You should have received a copy of the GNU General Public License
 * along with this application.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 // application runs in the Framework namespace
 namespace Framework;
 
-// Bootstrap framework and application settings
-include_once 'class/Framework/Config.php';
-Config::setPath(__DIR__ . '/config/');
-
-// Load the class autoloader
-include Config::get('framework', 'includePath') . 'autoload.php';
-
-// set timezone
-date_default_timezone_set(Config::get('application', 'timezone'));
-
-// start user session
-session_name(Config::get('application', 'sessionName'));
-session_start();
+// bootstrap the framework
+include_once 'bootstrap.php';
 
 // load the requested view using the standard preprocessor and layout
 Router::loadView();
